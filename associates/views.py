55833864481d90127aafe211dab0e9628dd6644e
nischay999 <nischay999@gmail.com>
2012-08-30 20:06:11 +0530

    Update associates/views.py

--- associates/views.py
+++ associates/views.py
@@ -1,1 +1,6 @@
 # Create your views here.
+def sponsors(request,domain,num):
+  event = get_event(domain,num)
+	sponsors = Sponsor.objects.filter(event = event)
+
+	return render_to_response(url +'sponsors.html',{'name':'Sponsors','list':create_events_menu(event),'event':event,'sponsors':sponsors},context_instance=RequestContext(request))
